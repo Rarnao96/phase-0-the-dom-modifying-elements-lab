96b92dc18e6ed9a7d835af5a17100e03087ce6c4
Rachel Arnao <rachel.e.arnao@gmail.com>
2023-04-21 22:56:52 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code here!
+//const newHeader = document.createElement("h1");
+const newHeader = document.getElementById("victory");
+newHeader.innerHTML = "<h1>Rachel is the champion</h1>"
